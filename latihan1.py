8ba989cf50fa1c7feded8c2bfa30bf10880f55b1
rizwan523 <46735698+rizwan523@users.noreply.github.com>
2019-02-18 11:28:28 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,7 @@
+from random import random
+print("masukan nilai N: 5")
+a=random()
+while (a<0.1):
+   a= a+1
+for i in range (1,6):
+	print("data ke:",i,"=>",a)
---
+++ latihan2.py
@@ -0,0 +1,8 @@
+max=0
+while True:
+	a= int(input("masukan bilagan : "))
+	if a==0:
+		break
+	if a>max:
+		max=a
+print("bilanga terbesar ",max)
